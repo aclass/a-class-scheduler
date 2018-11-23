f28a253aca22fb8c65523da2073bf54185adcbbd
Sugaya <sugaya324@gmail.com>
2018-11-23 14:36:21 +0900

    pushの時に追加されるidから削除を行う

--- src/components/DestinationList.vue
+++ src/components/DestinationList.vue
@@ -17,30 +17,72 @@ export default {
   name: 'destination-list',
   data() {
     return {
-      destinations: [
-        { destination: '金沢', count: 0 },
-        { destination: '札幌', count: 0 },
-        { destination: '名古屋', count: 0 },
-        { destination: '熊本', count: 0 }
-      ]
+      destinations: []
     };
   },
   methods: {
-    increment: function(index) {
-      this.destinations[index].count += 1;
+    increment: function(index, key) {
+      // this.destinations[index].count += 1;
+
+      firebase
+        .database()
+        .ref('sg/dest/')
+        .update({
+          count: (this.destinations[index].count += 1)
+        });
     },
     deleteList: function(index) {
-      this.destinations.splice(index, 1);
+      // this.destinations.splice(index, 1);
+      firebase
+        .database()
+        .ref('sg/dest/' + this.getKey(index))
+        .remove();
+    },
+    listen: function() {
+      // dbにデータが存在する場合はデータを取得する
+      if (true) {
+        console.log('listening…');
+        firebase
+          .database()
+          .ref('sg/dest/')
+          .on('value', snapshot => {
+            if (snapshot) {
+              const rootList = snapshot.val();
+              let list = [];
+              Object.keys(rootList).forEach((val, key) => {
+                rootList[val].id = val;
+                list.push(rootList[val]);
+              });
+              this.destinations = list;
+            }
+            console.log(this.destinations.length);
+          });
+      }
+    },
+    getKey: function(index) {
+      console.log(`id:${this.destinations[index].id}`);
+      return this.destinations[index].id;
     }
   },
-  mounted() {
+  created() {
     EventBus.$on('destination-add', destination => {
-      console.log('ok');
-      this.destinations.push({
-        destination: destination,
-        count: 0
-      });
+      console.log('destination-add ok');
+      // this.destinations.push({
+      //   destination: destination,
+      //   count: 0
+      // });
+      if (this.destinations !== null) {
+        firebase
+          .database()
+          .ref('sg/dest/')
+          .push({
+            destination: destination,
+            count: 0,
+            date: Date.now()
+          });
+      }
     });
+    this.listen();
   }
 };
 </script>
